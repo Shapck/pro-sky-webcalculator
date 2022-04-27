ed03ed8a0b7f0b6f25be09bbd8557c30713ab68f
Дима <dimonsoldatov@gmsil.com>
2022-04-27 23:19:33 +0300

    dz

---
+++ src/main/java/proskywebcalculator/CalculatorService.java
@@ -0,0 +1,16 @@
+package proskywebcalculator;
+
+public interface CalculatorService {
+    String hello();
+
+    String answerWelcomee();
+
+    String numPlus(Integer num1, Integer num2);
+
+    String numMinus(Integer num1, Integer num2);
+
+    String numMultiply(Integer num1, Integer num2);
+
+    String numDivide(Double num1, Double num2);
+
+}
