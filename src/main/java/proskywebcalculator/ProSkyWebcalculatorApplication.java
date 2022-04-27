75715ed6bd65b24e2d93a7b23092567b2de2b271
Дима <dimonsoldatov@gmsil.com>
2022-04-27 23:12:10 +0300

    dz

---
+++ src/main/java/proskywebcalculator/ProSkyWebcalculatorApplication.java
@@ -0,0 +1,13 @@
+package proskywebcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProSkyWebcalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProSkyWebcalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/proskywebcalculator/ProSkyWebcalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package proskywebcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProSkyWebcalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
